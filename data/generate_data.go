c9b87b1c3dd87d7e57af5bbd62e8b41b10dd5627
Geonwoo Kim <78521993+kgw7401@users.noreply.github.com>
2023-10-05 17:16:03 +0900

    Feat/event data generator (#7)
    * ✨ add:  이벤트 생성기 추가
    
    * view_home
    * view_searchResult
    
    * ✨ add:  api server로 데이터 전송
    
    * ✨ add:  ViewSearchResult 랜덤성 부여

--- data/generate_data.go
+++ data/generate_data.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"math/rand"
 	"net/http"
 
 	"github.com/go-faker/faker/v4"
@@ -16,12 +17,16 @@ func sendMessage(data []byte) {
 	}
 }
 
+func maybeSetField(probability float64) bool {
+	return rand.Float64() < probability
+}
+
 // view_home
 type ViewHome struct {
-	EventName string `json:"event_name"`
-	UserID    string `faker:"username" json:"user_id"`
-	DeviceID  string `faker:"uuid_hyphenated" json:"device_id"`
-	Platform  string `faker:"oneof: web, ios, android" json:"platform"`
+	EventName string `json:"event_name,omitempty"`
+	UserID    string `faker:"username" json:"user_id,omitempty"`
+	DeviceID  string `faker:"uuid_hyphenated" json:"device_id,omitempty"`
+	Platform  string `faker:"oneof: web, ios, android" json:"platform,omitempty"`
 }
 
 func GenerateViewHome() (data []byte) {
@@ -30,6 +35,7 @@ func GenerateViewHome() (data []byte) {
 	if err != nil {
 		panic(err)
 	}
+
 	ViewHome.EventName = "view_home"
 
 	data, err = json.Marshal(ViewHome)
@@ -41,43 +47,74 @@ func GenerateViewHome() (data []byte) {
 
 // view_searchResult
 type YearRange struct {
-	From int `faker:"boundary_start=1980, boundary_end=2000" json:"from"`
-	To   int `faker:"boundary_start=2001, boundary_end=2022" json:"to"`
+	From int `faker:"boundary_start=1980, boundary_end=2000" json:"from,omitempty"`
+	To   int `faker:"boundary_start=2001, boundary_end=2022" json:"to,omitempty"`
 }
 
 type PriceRange struct {
-	From int `faker:"boundary_start=1000000, boundary_end=5000000" json:"from"`
-	To   int `faker:"boundary_start=5000000, boundary_end=50000000" json:"to"`
+	From int `faker:"boundary_start=1000000, boundary_end=5000000" json:"from,omitempty"`
+	To   int `faker:"boundary_start=5000000, boundary_end=50000000" json:"to,omitempty"`
 }
 
 type Filter struct {
-	Segment   string     `faker:"oneof: SUV, RV, 버스, 경차, 소형차, 준중형차, 대형차, 스포츠카, 승합차" json:"segment"`
-	Fuel      string     `faker:"oneof: 가솔린, 디젤, LPG, 하이브리드, 전기" json:"fuel"`
-	Region    string     `faker:"oneof: 서울, 경기, 인천, 강원, 충북, 충남, 대전, 경북, 경남, 대구, 부산, 울산, 전북, 전남, 광주, 제주" json:"region"`
-	Color     string     `faker:"oneof: 검정, 흰색, 은색, 회색, 빨강, 주황, 노랑, 초록, 파랑, 남색, 보라, 갈색, 기타" json:"color"`
-	YearType  YearRange  `json:"year_type"`
-	PriceType PriceRange `json:"price_type"`
+	Segment   string      `faker:"oneof: SUV, RV, 버스, 경차, 소형차, 준중형차, 대형차, 스포츠카, 승합차" json:"segment,omitempty"`
+	Fuel      string      `faker:"oneof: 가솔린, 디젤, LPG, 하이브리드, 전기" json:"fuel,omitempty"`
+	Region    string      `faker:"oneof: 서울, 경기, 인천, 강원, 충북, 충남, 대전, 경북, 경남, 대구, 부산, 울산, 전북, 전남, 광주, 제주" json:"region,omitempty"`
+	Color     string      `faker:"oneof: 검정, 흰색, 은색, 회색, 빨강, 주황, 노랑, 초록, 파랑, 남색, 보라, 갈색, 기타" json:"color,omitempty"`
+	YearType  *YearRange  `json:"year_type,omitempty"`
+	PriceType *PriceRange `json:"price_type,omitempty"`
 }
 
 type Parameters struct {
-	Filter Filter `json:"filter"`
+	Filter Filter `json:"filter,omitempty"`
 }
 
 type ViewSearchResult struct {
-	EventName  string     `json:"event_name"`
-	UserID     string     `faker:"username" json:"user_id"`
-	DeviceID   string     `faker:"uuid_hyphenated" json:"device_id"`
-	Platform   string     `faker:"oneof: web, ios, android" json:"platform"`
-	Parameters Parameters `json:"parameters"`
+	EventName  string      `json:"event_name,omitempty"`
+	UserID     string      `faker:"username" json:"user_id,omitempty"`
+	DeviceID   string      `faker:"uuid_hyphenated" json:"device_id,omitempty"`
+	Platform   string      `faker:"oneof: web, ios, android" json:"platform,omitempty"`
+	Parameters *Parameters `json:"parameters,omitempty"`
+}
+
+func GenerateFilter(filter *Filter) Filter {
+	if maybeSetField(0.9) {
+		err := faker.FakeData(&filter)
+		if err != nil {
+			panic(err)
+		}
+		if maybeSetField(0.8) {
+			yearRange := YearRange{}
+			err := faker.FakeData(&yearRange)
+			if err != nil {
+				panic(err)
+			}
+			filter.YearType = &yearRange
+		}
+		if maybeSetField(0.8) {
+			priceRange := PriceRange{}
+			err := faker.FakeData(&priceRange)
+			if err != nil {
+				panic(err)
+			}
+			filter.PriceType = &priceRange
+		}
+	} else {
+		return *filter
+	}
+	return *filter
 }
 
 func GenerateViewSearchResult() (data []byte) {
-	ViewSearchResult := ViewSearchResult{}
+	filter := Filter{}
+	generatedFilter := GenerateFilter(&filter)
+	ViewSearchResult := &ViewSearchResult{}
 	err := faker.FakeData(&ViewSearchResult)
 	if err != nil {
 		panic(err)
 	}
 	ViewSearchResult.EventName = "view_searchResult"
+	ViewSearchResult.Parameters.Filter = generatedFilter
 
 	data, err = json.Marshal(ViewSearchResult)
 	if err != nil {
@@ -88,8 +125,7 @@ func GenerateViewSearchResult() (data []byte) {
 
 func main() {
 	viewHomeData := GenerateViewHome()
-	viewSerachResult := GenerateViewSearchResult()
-
 	sendMessage(viewHomeData)
-	sendMessage(viewSerachResult)
+	viewSearchResultData := GenerateViewSearchResult()
+	sendMessage(viewSearchResultData)
 }
