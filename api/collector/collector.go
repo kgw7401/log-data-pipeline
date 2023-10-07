93f6acfaf93cbc183d521d116d472bf5178fc5df
geonwoo <kgw7401@gmail.com>
2023-10-07 19:13:33 +0900

    ✨ add:  collectorHandler 구현
    * json 데이터 받기
    * validation 기능

--- api/collector/collector.go
+++ api/collector/collector.go
@@ -1,16 +1,35 @@
 package collector
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
+
+	"github.com/xeipuuv/gojsonschema"
 )
 
 func collectHandler(w http.ResponseWriter, r *http.Request) {
-	body, err := io.ReadAll(r.Body)
+	data, _ := io.ReadAll(r.Body)
+	unmarshalData := data
+	var resultMap map[string]interface{}
+	json.Unmarshal(unmarshalData, &resultMap)
+	jsonFile := fmt.Sprintf("file:///Users/kgw7401/log-data-pipeline/api/collector/schema/%s.json", resultMap["event_name"])
+
+	schemaLoader := gojsonschema.NewReferenceLoader(jsonFile)
+	documentLoader := gojsonschema.NewBytesLoader(data)
+	result, err := gojsonschema.Validate(schemaLoader, documentLoader)
 	if err != nil {
-		panic(err)
+		panic(err.Error())
 	}
-	println(string(body))
+	if result.Valid() {
+		fmt.Printf("The document is valid\n")
+	} else {
+		fmt.Printf("The document is not valid. see errors :\n")
+		for _, err := range result.Errors() {
+			fmt.Printf("- %s\n", err)
+		}
+	}
 }
 
 func NewHttpHandler() http.Handler {
