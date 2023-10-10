0dc643a1b558e7de0854696d71ec1bf9f80cc750
Geonwoo Kim <78521993+kgw7401@users.noreply.github.com>
2023-10-10 23:14:30 +0900

    ✨ add:  카프카 프로듀서, 컨슈머 구현 (#8)

--- api/collector/collector.go
+++ api/collector/collector.go
@@ -2,34 +2,18 @@ package collector
 
 import (
 	"encoding/json"
-	"fmt"
 	"io"
 	"net/http"
 
-	"github.com/xeipuuv/gojsonschema"
+	"github.com/log-data-pipeline/api/kafka"
 )
 
 func collectHandler(w http.ResponseWriter, r *http.Request) {
 	data, _ := io.ReadAll(r.Body)
-	unmarshalData := data
-	var resultMap map[string]interface{}
-	json.Unmarshal(unmarshalData, &resultMap)
-	jsonFile := fmt.Sprintf("file:///Users/kgw7401/log-data-pipeline/api/collector/schema/%s.json", resultMap["event_name"])
-
-	schemaLoader := gojsonschema.NewReferenceLoader(jsonFile)
-	documentLoader := gojsonschema.NewBytesLoader(data)
-	result, err := gojsonschema.Validate(schemaLoader, documentLoader)
-	if err != nil {
-		panic(err.Error())
-	}
-	if result.Valid() {
-		fmt.Printf("The document is valid\n")
-	} else {
-		fmt.Printf("The document is not valid. see errors :\n")
-		for _, err := range result.Errors() {
-			fmt.Printf("- %s\n", err)
-		}
-	}
+	result := make(map[string]string)
+	_ = json.Unmarshal(data, &result)
+	eventName := result["event_name"]
+	kafka.ProduceData(eventName, data)
 }
 
 func NewHttpHandler() http.Handler {
---
+++ consumer/go.mod
@@ -0,0 +1,15 @@
+module github.com/log-data-pipeline/consumer
+
+go 1.21.0
+
+require (
+	github.com/confluentinc/confluent-kafka-go/v2 v2.2.0
+	github.com/riferrei/srclient v0.6.0
+)
+
+require (
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/linkedin/goavro/v2 v2.11.1 // indirect
+	github.com/santhosh-tekuri/jsonschema/v5 v5.2.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+)
